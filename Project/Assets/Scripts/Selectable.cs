39e769aa89a3bc94c58ed640d9b437186226657f
masakikaihori <nana7hachi89@gmail.com>
2026-03-25 19:14:07 +0900

    feat: 選択中のオブジェクトを赤くハイライト #4

--- Project/Assets/Scripts/Selectable.cs
+++ Project/Assets/Scripts/Selectable.cs
@@ -6,7 +6,11 @@ public class Selectable : MonoBehaviour
     // Inspector で明示的に指定も可能（空なら子階層を自動探索）
     [SerializeField] private Collider targetCollider;
 
-    private Color originalColor;
+    private Renderer[] targetRenderers;
+
+    private Material[][] cachedMaterials;
+    
+    private Color[][] originalColors;
 
     private void Awake()
     {
@@ -14,18 +18,55 @@ public class Selectable : MonoBehaviour
             targetCollider = GetComponentInChildren<Collider>();
         
         Debug.Log("targetColider: "+targetCollider);
+
+        targetRenderers = GetComponentsInChildren<Renderer>();
+
+        cachedMaterials = new Material[targetRenderers.Length][];
+        originalColors = new Color[targetRenderers.Length][];
+
+        for (int i = 0; i < targetRenderers.Length; i++)
+        {
+            cachedMaterials[i] = targetRenderers[i].materials;
+            
+            originalColors[i] = new Color[cachedMaterials[i].Length];
+
+            for (int j = 0; j < cachedMaterials[i].Length; j++)
+            {
+                originalColors[i][j] = cachedMaterials[i][j].color;
+            }
+        }
     }
 
     public void OnSelected()
     {
+        if (cachedMaterials == null) return;
+
+        for (int i = 0; i < cachedMaterials.Length; i++)
+        {
+            for (int j = 0; j < cachedMaterials[i].Length; j++)
+            {
+                cachedMaterials[i][j].color = Color.red; 
+            }
+        }
     }
 
     public void OnDeselected()
     {
+        if (cachedMaterials == null || originalColors == null) return;
+
+        Debug.Log("選択解除されました");
+
+        for (int i = 0; i < cachedMaterials.Length; i++)
+        {
+            for (int j = 0; j < cachedMaterials[i].Length; j++)
+            {
+                cachedMaterials[i][j].color = originalColors[i][j]; 
+            }
+        }
     }
 
     public void OnDropped()
     {
-        // ドロップ時の処理（必要に応じてカスタマイズ）
+        Debug.Log(gameObject.name + " がドロップされました");
     }
 }
